343fdfe0bcc823baafbf76c8e457320052c050af
Marc Wäckerlin <marc@waeckerlin.org>
2016-11-22 16:49:09 +0100

    docu updated

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 var crypto = require('crypto');
 var LdapAuth = require('ldapauth');
-/** authenticate users either by ldap or username password, according to configuration
-
-   without configuration, access is unrestricted
-
-   example configuration
-   @pre
-   {
-     "passwords": {
-       "foo": ["sha256", "fcde2b2edxx56bf408601fb721fe9b5c338d10ee429ea04fae5511b68fbf8fb9"]
-     },
-     "ldap": {
-       "url": "ldap://dev.marc.waeckerlin.org",
-       "adminDn": "cn=tmp,ou=system,ou=people,dc=dev,dc=marc,dc=waeckerlin,dc=org",
-       "adminPassword": "secret",
-       "searchBase": "ou=person,ou=people,dc=dev,dc=marc,dc=waeckerlin,dc=org",
-       "searchFilter": "(uid={{username}})"
-     }
-   }
-   @pre
- 
-   @param  config json configuration
-   @return function(username, password, success, fail)
- */
 module.exports = function(config) {
 
   var authentication;
